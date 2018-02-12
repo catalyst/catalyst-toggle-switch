639162f4ead44ec1fe839ddd04ad328de514fd77
Rebecca Stevens <rebeccastevens@catalyst.net.nz>
2018-02-12 14:45:10 +1300

    Switched to using Polymer 3 preview elements for the docs.

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -3,7 +3,20 @@
 # Only run if not in node_modules.
 if [ \"${PWD##*/}\" != node_modules ]
 then
-  # Create bower_components symlink.
-  # Needed for running the web component tester on the command line.
-  ln -sf ./node_modules/@bower_components ./bower_components
+  # Fix bad dependency links
+  # These should only be temp fixes until Polymer 3.0 is released.
+
+  # `prism` should be `prismjs`
+  ln -sf ./prismjs ./node_modules/prism
+
+  # `test-fixture` is being loaded from the wrong place.
+  ln -sf ./@polymer/test-fixture ./node_modules/test-fixture
+
+  # Fix lint to async.js
+  mkdir -p ./node_modules/async/lib
+  ln -sf ../dist/async.js ./node_modules/async/lib/async.js
+
+  # Fix linl to sinon.js
+  mkdir -p ./node_modules/sinonjs
+  ln -sf ../sinon/pkg/sinon.js ./node_modules/sinonjs/sinon.js
 fi
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-wct=node_modules/@bower_components/web-component-tester/bin/wct
+wct=node_modules/web-component-tester/bin/wct
 
 # detect if xvfb-run is present
 if [[ -x `command -v xvfb-run` ]]; then
-  xvfb-run node $wct
+  xvfb-run node $wct --npm
 else
-  node $wct
+  node $wct --npm
 fi
