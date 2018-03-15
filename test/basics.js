d05475f790aaa49ba6a295f368c99ee181cb5060
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2018-03-15 16:25:03 +1300

    Updated test to be in seperate js files.

---
+++ test/basics.js
@@ -0,0 +1,166 @@
+/**
+ * Basics suite.
+ */
+suite('Basics', () => {
+  let element;
+
+  /**
+   * Called before each following tests is run.
+   */
+  setup(() => {
+    element = fixture('basic');
+  });
+
+  /**
+   * Test the initial state of the element.
+   */
+  suite('Initial State', () => {
+    /**
+     * Test the attributes and properties of the element in it's initial state.
+     */
+    suite('Attributes & Properties', () => {
+      /**
+       * Test checked.
+       */
+      test('checked', () => {
+        expect(element).to.have.property('checked', false);
+        expect(element.hasAttribute('checked')).to.be.false;
+      });
+    });
+
+    /**
+     * Test the accessibility of the element in it's initial state.
+     */
+    suite('Accessibility', () => {
+      /**
+       * Test role.
+       */
+      test('role', () => {
+        // role
+        expect(element.hasAttribute('role'), '"role" missing').to.be.true;
+        expect(element.getAttribute('role')).to.equal(
+          'checkbox',
+          '"role" should be set to "checkbox"'
+        );
+      });
+
+      /**
+       * Test tabindex.
+       */
+      test('tabindex', () => {
+        expect(element.hasAttribute('tabindex'), '"tabindex" missing').to.be
+          .true;
+        expect(Number.parseInt(element.getAttribute('tabindex'))).to.equal(
+          0,
+          '"tabindex" should be 0'
+        );
+      });
+
+      /**
+       * Test aria-checked.
+       */
+      test('aria-checked', () => {
+        expect(element.hasAttribute('aria-checked'), '"aria-checked" missing')
+          .to.be.true;
+        expect(element.getAttribute('aria-checked')).to.equal('false');
+      });
+
+      /**
+       * Test aria-disabled.
+       */
+      test('aria-disabled', () => {
+        expect(element.hasAttribute('aria-disabled'), '"aria-disabled" missing')
+          .to.be.true;
+        expect(element.getAttribute('aria-disabled')).to.equal('false');
+      });
+    });
+  });
+
+  /**
+   * Test the attributes and properties of the element in it's initial state.
+   */
+  suite('Changing State', () => {
+    /**
+     * Mouse Interaction.
+     */
+    suite('Mouse Interaction', () => {
+      /**
+       * Test clicking the element.
+       */
+      test('Click Once', done => {
+        let testRunning = true;
+
+        // Change event needs to be fired.
+        element.addEventListener('change', () => {
+          if (testRunning) {
+            testRunning = false;
+            done();
+          }
+        });
+
+        element.click();
+
+        // Values that should change.
+        expect(element.hasAttribute('checked')).to.be.true;
+        expect(element).to.have.property('checked', true);
+        expect(element.getAttribute('aria-checked')).to.equal('true');
+
+        setTimeout(function() {
+          if (testRunning) {
+            testRunning = false;
+            assert(false, 'Change event was not fired within 10 ms.');
+            done();
+          }
+        }, 10);
+      });
+
+      /**
+       * Test clicking the element twice.
+       */
+      test('Click Twice', () => {
+        element.click();
+        element.click();
+
+        expect(element.hasAttribute('checked')).to.be.false;
+        expect(element).to.have.property('checked', false);
+        expect(element.getAttribute('aria-checked')).to.equal('false');
+      });
+
+      /**
+       * Test clicking the element when disabled.
+       */
+      test('Click When Disabled', done => {
+        let testRunning = true;
+        element.disabled = true;
+
+        // Change event should not be fired.
+        element.addEventListener('change', () => {
+          if (testRunning) {
+            testRunning = false;
+            assert(false, 'Change event should not be fired.');
+            done();
+          }
+        });
+
+        element.click();
+
+        expect(element.hasAttribute('checked')).to.be.false;
+        expect(element).to.have.property('checked', false);
+        expect(element.getAttribute('aria-checked')).to.equal('false');
+
+        setTimeout(function() {
+          if (testRunning) {
+            testRunning = false;
+            done();
+          }
+        }, 10);
+      });
+    });
+
+    /**
+     * KeyBoard Interaction.
+     * TODO: write tests.
+     */
+    suite('KeyBoard Interaction', () => {});
+  });
+});
---
+++ test/form.js
@@ -0,0 +1,63 @@
+/**
+ * Form suite.
+ */
+suite('Form', () => {
+  let form;
+  let element;
+
+  /**
+   * Called before each following test is run.
+   */
+  setup(() => {
+    form = fixture('form');
+    element = form.querySelector('catalyst-toggle-switch');
+  });
+
+  /**
+   * Test the initial state of the element.
+   */
+  suite('Initial State', () => {
+    /**
+     * Test the attributes and properties of the element in it's initial state.
+     */
+    suite('Attributes & Properties', () => {
+      /**
+       * Test element has name.
+       */
+      test('name', () => {
+        expect(element.hasAttribute('name')).to.be.true;
+        expect(element.getAttribute('name')).to.equal('foo');
+        expect(element).to.have.property('name', 'foo');
+        expect(element.inputElement).to.have.property('name', 'foo');
+      });
+
+      /**
+       * Test element has value.
+       */
+      test('value', () => {
+        expect(element).to.have.property('value', 'bar');
+        expect(element.inputElement).to.have.property('value', 'bar');
+      });
+
+      /**
+       * Test element has form.
+       */
+      test('form', () => {
+        expect(element).to.have.property('form', form);
+        expect(element.inputElement).to.have.property('form', form);
+      });
+    });
+
+    /**
+     * Test the form.
+     */
+    suite('Form', () => {
+      /**
+       * Test form has element.
+       */
+      test('Element is part of the form', () => {
+        expect(form.elements['foo']).to.exist;
+      });
+    });
+  });
+});
